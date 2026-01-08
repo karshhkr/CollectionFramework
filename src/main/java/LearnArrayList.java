a5a446e2a7e3dd6b4b208ec7111d9cd3471b00e4
karshhkr <utkarshkumardabgarwal@gmail.com>
2026-01-08 16:44:00 +0530

    Initial commit

---
+++ src/main/java/LearnArrayList.java
@@ -0,0 +1,13 @@
+
+
+ import java.util.ArrayList;
+ public class ArrayList {
+    public static void main(String[] args) {
+//      String[] studentsName= new String[30];
+//      studentsName[0]="Karshhkr";
+//
+//      studentsName[28]="Utkarshhkr";
+//      studentsName[30]="Kiwi";
+        ArrayList
+    }
+}
